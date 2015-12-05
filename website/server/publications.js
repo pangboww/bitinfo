897117b7cf6e23d03204726a76ba8c90df820f08
pangboww <pangboww@gmail.com>
2015-12-05 01:54:55 +0100

    add all/hour/day/week sort for database

--- website/server/publications.js
+++ website/server/publications.js
@@ -2,6 +2,40 @@
  * Created by pangboww on 03/12/15.
  */
 
-Meteor.publish('btcc', function(options){
-    return Btcc.find({}, options)
+Meteor.publish('btcc_all', function(options){
+    return Btcc.find({}, options);
+});
+
+Meteor.publish('btcc_hour', function(options){
+    options.limit = 60;
+    return Btcc.find({}, options);
+});
+
+Meteor.publish('btcc_day', function(options){
+    options.limit = 60 * 24;
+    return Btcc.find({}, options);
+});
+
+Meteor.publish('btcc_week', function(options){
+    options.limit = 60 * 24 * 7;
+    return Btcc.find({}, options);
+});
+
+Meteor.publish('huobi_all', function(options){
+    return Huobi.find({}, options);
+});
+
+Meteor.publish('huobi_hour', function(options){
+    options.limit = 60;
+    return Huobi.find({}, options);
+});
+
+Meteor.publish('huobi_day', function(options){
+    options.limit = 60 * 24;
+    return Huobi.find({}, options);
+});
+
+Meteor.publish('huobi_week', function(options){
+    options.limit = 60 * 24 * 7;
+    return Huobi.find({}, options);
 });
